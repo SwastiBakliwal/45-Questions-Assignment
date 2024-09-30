12379051e81855186c650a46cba5285cdd66ae77
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-09-30 21:47:46 +0530

    Add files via upload

---
+++ ques2.java
@@ -0,0 +1,11 @@
+class ques2 
+{
+    public static void main(String[] args) 
+    {
+        int area = 96;
+        int breadth = 8;
+        
+        System.out.println("Length of rectangle is "+(area/breadth)+" cm");
+        System.out.println("Perimeter of rectangle is "+(2*(breadth+(area/breadth)))+" cm");
+    }
+}

013b73b26661e5aecdb9be7efb3c09b926aba58a
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-01 16:49:07 +0530

    Add files via upload

---
+++ ques7.java
@@ -0,0 +1,10 @@
+class ques7 
+{
+ public static void main(String[] args) 
+ {
+   int base = 50;
+   int area = 500;
+   int height = (2*area)/base;
+   System.out.println("The of the triangle is "+height+" cm");     
+ }    
+}

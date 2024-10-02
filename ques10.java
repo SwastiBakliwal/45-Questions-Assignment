9078d95780954d4be7b1abe7651117786556c15b
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-02 20:35:08 +0530

    Add files via upload

---
+++ ques10.java
@@ -0,0 +1,10 @@
+class ques10 
+{
+    public static void main(String[] args) 
+    {
+      float sides = 15;
+      Float area = (sides*sides)/2; 
+       
+      System.out.println("The area is "+area+" cm^2");
+    }
+}

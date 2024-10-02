a8a384362d8dacb6bc5ba8109660f77eddc0b0bd
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-02 20:34:49 +0530

    Add files via upload

---
+++ ques9.java
@@ -0,0 +1,9 @@
+class ques9 
+{
+    public static void main(String[] args) 
+    {
+      int s = 12;
+      double area = (Math.sqrt(3)/4)*s*s;
+      System.out.println("The Area of Equilateral triangle is  "+area+" cm^2");     
+    }  
+}

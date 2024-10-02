e4a5a08437675d307054bd0958c87182c8e81967
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-02 20:34:29 +0530

    Add files via upload

---
+++ ques8.java
@@ -0,0 +1,10 @@
+class ques8 
+{
+public static void main(String[] args) 
+{
+   int area = 8000;
+   int altitude = 20;
+   int base = (area*2)/altitude;
+   System.out.println("The base of the triangle is "+base+" cm");   
+}    
+}

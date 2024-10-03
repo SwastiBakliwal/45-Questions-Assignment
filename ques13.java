c118e3f06cc3f29d593af96cf8d0221ea7361248
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-03 18:16:57 +0530

    Add files via upload

---
+++ ques13.java
@@ -0,0 +1,10 @@
+class ques13 
+{
+ public static void main(String[] args) {
+    double area = 184;
+        double leg1 = 16;
+        double leg2 = (2 * area) / leg1;
+        
+        System.out.println("The length of the other leg is: " + leg2 + " cm");
+ }    
+}

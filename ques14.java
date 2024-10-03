3f8db015a46464218b81954127b1a8f4b2357f7e
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-03 18:17:21 +0530

    Add files via upload

---
+++ ques14.java
@@ -0,0 +1,15 @@
+class ques14 
+{
+    public static void main(String[] args) {
+        double diagonal = 46;
+        double height1 = 13;
+        double height2 = 10;
+        
+        double area1 = 0.5 * diagonal * height1;
+        double area2 = 0.5 * diagonal * height2;
+        
+        double totalArea = area1 + area2;
+        
+        System.out.println("The area of the field is: " + totalArea + " m^2");
+    }   
+}

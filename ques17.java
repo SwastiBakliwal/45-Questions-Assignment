b239a77ab90bab9ecdb716772bfd62ae8767da78
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-04 21:43:29 +0530

    Add files via upload

---
+++ ques17.java
@@ -0,0 +1,14 @@
+class ques17 
+{
+    public static void main(String[] args) {
+        
+        double length = 3;
+        double width = 2;
+        
+        
+        double perimeter = 2 * (length + width);
+        
+        
+        System.out.println("The length of ribbon needed is: " + perimeter + " meters");
+    }
+}

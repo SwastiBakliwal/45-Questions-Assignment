da2c03d806a5204824f6b0689aabf7b360bb22f2
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-04 21:43:14 +0530

    Add files via upload

---
+++ ques16.java
@@ -0,0 +1,14 @@
+class ques16 
+{
+    public static void main(String[] args) {
+    
+        double length = 5;
+        double width = 7;
+        
+    
+        double area = length * width;
+        
+    
+        System.out.println("The area of the dining room is: " + area + " m^2");
+    }    
+}

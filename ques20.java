231b83382c9e3e472157d3d827a67dc78321000d
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-05 17:11:32 +0530

    Add files via upload

---
+++ ques20.java
@@ -0,0 +1,15 @@
+class ques20 
+{
+    public static void main(String[] args) {
+        
+        double length = 25;
+        double breadth = 10;
+        double height = 8;
+        
+        
+        double volume = length * breadth * height;
+        
+        
+        System.out.println("The volume of the brick of ice-cream is: " + volume + " cm^3");
+    }
+}

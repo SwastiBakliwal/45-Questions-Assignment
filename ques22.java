d75961a1eb784e3f82e7824569975d673ef3b690
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-05 17:12:05 +0530

    Add files via upload

---
+++ ques22.java
@@ -0,0 +1,15 @@
+class ques22 
+{
+    public static void main(String[] args) {
+    
+        double length = 50.0;
+        double width = 30.0;
+        double depth = 2.0;
+
+    
+        double volume = length * width * depth;
+
+    
+        System.out.println("The capacity of the pond is: " + volume + " cubic meters");
+    }    
+}

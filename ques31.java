8cc91f494293dcfc4522da555faf32ee0b7eafb9
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-07 21:05:55 +0530

    Add files via upload

---
+++ ques31.java
@@ -0,0 +1,11 @@
+class ques31 
+{
+   public static void main(String[] args) {
+    int perimeterofsquare = 200;
+    int side = perimeterofsquare/4;
+    int area = side * side;
+
+    System.out.println("The side of square is "+side+"m");
+    System.out.println("The Area of Square is "+area+" m^2");
+   }    
+}

6eaa931fde5085beeffe9324b6c568792440b145
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-07 21:04:54 +0530

    Add files via upload

---
+++ ques28.java
@@ -0,0 +1,19 @@
+class ques28 
+{
+    public static void main(String[] args) {
+        
+        double length = 5;
+        double width = 4;
+
+    
+        double pricePerSquareMeter = 205;
+
+        
+        double area = length * width;
+
+        
+        double price = area * pricePerSquareMeter;
+
+        System.out.println("The price of the carpet is: $" + price);
+    }
+}

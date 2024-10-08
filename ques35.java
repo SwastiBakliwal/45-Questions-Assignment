e170a0fb97a51e3ddc5dd9e5e512f2a0073ef0e1
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-08 21:36:10 +0530

    Add files via upload

---
+++ ques35.java
@@ -0,0 +1,11 @@
+class ques35 
+{
+    public static void main(String[] args) {
+        double radius = 7.7;
+        double height = 12;
+
+        double surfacearea = 2 * Math.PI * radius * (radius + height);
+
+        System.out.println("The surface area of the cylinder is: " + surfacearea + " square cm");
+    }
+}

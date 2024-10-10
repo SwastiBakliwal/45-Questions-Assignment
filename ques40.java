24c299fe5a2ccc4ae669ba849caa1ca589ec893e
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-10 19:35:01 +0530

    Add files via upload

---
+++ ques40.java
@@ -0,0 +1,14 @@
+class ques40 
+{
+    public static void main(String[] args) {
+        
+        double radius = 5.0; // in cm
+        double height = 9.0; // in cm
+
+        //pir2h
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+
+        
+        System.out.println("The volume of the cylinder is: " + volume + " cubic cm");
+    }
+}

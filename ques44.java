9d7d200bd9389875543608211f35c5c4c97f3c42
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-11 20:40:34 +0530

    Add files via upload

---
+++ ques44.java
@@ -0,0 +1,15 @@
+class ques44 
+{
+    public static void main(String[] args) {
+        
+        int a = -21; // First term
+        int d = 3; // Common difference
+        int n = 28; // Term number
+
+        
+        int nthTerm = a + (n - 1) * d;
+
+        
+        System.out.println("The 28th term of the AP is: " + nthTerm);
+    }
+}
